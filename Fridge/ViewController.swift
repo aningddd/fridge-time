e0936da0b45bfa97945ecbc007e11d8db34f786c
aningddd <shutong.luo.03@gmail.com>
2022-12-11 18:03:23 -0600

    Initial Commit

---
+++ Fridge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fridge
+//
+//  Created by aning on 11/12/22.
+//  Copyright © 2022 Shu Tong Luo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
