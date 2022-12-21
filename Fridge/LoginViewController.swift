46902e5b328c34a1a343986663c7d6898e3b9b6f
aningddd <shutong.luo.03@gmail.com>
2022-12-21 17:18:49 +1100

    ui stuff and login

---
+++ Fridge/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  Fridge
+//
+//  Created by aning on 21/12/22.
+//  Copyright © 2022 Shu Tong Luo. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func nextButtonPressed(_ sender: Any) {
+    }
+    
+}
